3e9fdbd12377a412be83ee22805ee089247f6d1d
HP-Pro6300 <1075605131@qq.com>
2019-04-10 22:23:09 +0800

    '完成地图导航功能'

--- taobao/views.py
+++ taobao/views.py
@@ -93,7 +93,6 @@ def navigation(request):
     origin = request.GET.get('origin')
     destination = request.GET.get('destination')
     way = request.GET.get('way')
-    print(origin, destination, way)
     return render(request, 'navigation.html', {'origin': origin, 'destination': destination, 'way': way})
 # def show(request, id):
 #     mydict = {}
