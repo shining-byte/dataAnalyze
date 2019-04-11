4dc4618c997a8a78366db8621c961e71813274da
HP-Pro6300 <1075605131@qq.com>
2019-04-11 19:22:55 +0800

    '完成商品搜索的响应式'

--- test.py
+++ test.py
@@ -2,19 +2,10 @@ import re
 from scrapy import Selector
 import requests
 
-url = 'https://s.taobao.com/search?q=华为'
+url = 'http://www.mafengwo.cn/hotel/ajax.php?iMddId=10099&iAreaId=-1&iRegionId=-1&iPoiId=&position_name=&nLat=0&nLng=0&iDistance=10000&sCheckIn=2019-05-19&sCheckOut=2019-05-20&iAdultsNum=2&iChildrenNum=0&sChildrenAge=&iPriceMin=&iPriceMax=&sTags=&sSortType=comment&sSortFlag=DESC&has_booking_rooms=0&has_faved=0&sKeyWord=&iPage=1&sAction=getPoiList5&_ts=1554907489439&_sn=f01809d556&_ts=1554907489440&_sn=fa07e689c1'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
            'authorization': 'oauth c3cef7c66a1843f8b3a9e6a1e3160e20'}
 response = requests.get(url=url, headers=headers)
-prices = re.findall('"view_price":"(.*?)",', response.text)[:5]  # 正则提示商品价格
-nid = re.findall('"nid":"(.*?)"', response.text)[:5]
-title = re.findall('"raw_title":"(.*?)"', response.text)[:5]
-pic_url = re.findall('"pic_url":"(.*?)"', response.text)[:5]
-detail_url = re.findall('"detail_url":"(.*?)"', response.text)[:5]
-list = []
-print(nid)
-for i in nid:
-    detail_url = ['https://detail.tmall.com/item.htm?spm=a230r.1.14.6.70de7ffcZej9q8&id={}&cm_id=140105335569ed55e27b&abbucket=20&sku_properties=10004:1617715035;5919063:6536025'.format(i)]
-    list.append(detail_url)
-print(list)
+response.text.encode('gbk')
+print(response.text)
